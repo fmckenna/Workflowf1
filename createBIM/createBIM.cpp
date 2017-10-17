b1af112f5abb18c2e05f02d7aa30a824caea7d29
fmckenna <fmckenna@berkeley.edu>
2017-10-17 16:34:29 -0700

    changes for structType

--- createBIM/createBIM.cpp
+++ createBIM/createBIM.cpp
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <sstream>
 
 #include "csvparser.h"
 #include <jansson.h>
 #include <map>
-
+#include <sstream>
+#include <fstream>
+using namespace std;
+const char* deterStructtype(int year, int bldtypeid, int story);
+const char* deteroccupancy(int building_type);
+double replacementcost(int building_type);
 
 struct locations {
   locations():x(0),y(0) {}
@@ -18,8 +22,187 @@ struct locations {
   double x;
   double y;
 };
+const char* deteroccupancy(int building_type){
+    const char * buildoccupancy;
+    //double replacementcost;
+    switch (building_type) {
+    case 1:
+        buildoccupancy="Residential";
+        break;
+    case 2:
+        buildoccupancy="Residential";
+        break;
+    case 3:
+        buildoccupancy="Residential";
+        break;
+    case 4:
+        buildoccupancy="Office";
+        break;
+    case 5:
+        buildoccupancy="Hotel";
+        break;
+    case 6:
+        buildoccupancy="School";
+        break;
+    case 7:
+        buildoccupancy="Industrial";
+        break;
+    case 8:
+        buildoccupancy="Industrial";
+        break;
+    case 9:
+        buildoccupancy="Industrial";
+        break;
+    case 10:
+        buildoccupancy="Retail";
+        break;
+    case 11:
+        buildoccupancy="Retail";
+        break;
+    case 12:
+        buildoccupancy="Residential";
+        break;
+    case 13:
+        buildoccupancy="Retail";
+        break;
+    case 14:
+        buildoccupancy="Office";
+        break;
+    default:
+        buildoccupancy="Residential";
+        break;
+    }
+    return buildoccupancy;
+}
+double replacementcost(int building_type){
+    //const char * buildoccupancy;
+    double replacementcost;
+    switch (building_type) {
+    case 1:
+        replacementcost=137.5452*(1+0.5);
+        break;
+    case 2:
+        replacementcost=137.5452*(1+0.5);
+        break;
+    case 3:
+        replacementcost=137.5452*(1+0.5);
+        break;
+    case 4:
+        replacementcost=131.8863*(1+1);
+        break;
+    case 5:
+        replacementcost=137.271225*(1+0.5);
+        break;
+    case 6:
+        replacementcost=142.134265*(1+1.25);
+        break;
+    case 7:
+        replacementcost=97.5247*(1+1.5);
+        break;
+    case 8:
+        replacementcost=85.9586*(1+1.5);
+        break;
+    case 9:
+        replacementcost=104.033475*(1+1.5);
+        break;
+    case 10:
+        replacementcost=105.33705*(1+1);
+        break;
+    case 11:
+        replacementcost=105.33705*(1+1);
+        break;
+    case 12:
+        replacementcost=137.5452*(1+0.5);
+        break;
+    case 13:
+        replacementcost=105.33705*(1+1);
+        break;
+    case 14:
+        replacementcost=131.8863*(1+1);
+        break;
+    default:
+        replacementcost=137.5452*(1+0.5);
+        break;
+    }
+    return replacementcost;
+}
+const char* deterStructtype(int year, int bldtypeid, int story)
+{
+    const char* buildingtype;
+    int structingtypeID;
+    if(year<=1900){
+        int rand_array[4]={1,7,8,9};
+        structingtypeID=rand_array[(rand() % (3-0+1))+ 0];
+    }
+    else if(year>1900){
+        if(story<4){
+            if(bldtypeid==1||bldtypeid==2||bldtypeid==3||bldtypeid==12){  //1-12 Residential 13-19 Commercial 20-22 Industrial 23-25 Other
+                structingtypeID=1;
+            }
+            else if(bldtypeid==4||bldtypeid==5||bldtypeid==6||bldtypeid==10||bldtypeid==11||bldtypeid==13||bldtypeid==14){
+                structingtypeID=(rand() % (8-1+1))+ 1; //Commercial
+            }
+            else if(bldtypeid<10&&bldtypeid>6){
+                structingtypeID=(rand() % (8-2+1))+ 2; //Industrial
+            }
+            else {
+                structingtypeID=1;
+            }
+
+        }
+        else if(story>3&&story<8){
+            if(bldtypeid<7&&bldtypeid>0||bldtypeid>9&&bldtypeid<15){  //1-12 Residential 13-19 Commercial 20-22 Industrial 23-25 Other
+                structingtypeID=(rand() % (5-1+1))+ 1;//Commercial and Residential
+            }
+            else if(bldtypeid<10&&bldtypeid>6){
+                structingtypeID=(rand() % (5-2+1))+ 2; //Industrial
+            }
+            else {
+                structingtypeID=(rand() % (5-1+1))+ 1;//other
+            }
+
+        }
+        else {
+            structingtypeID=(rand() % (5-2+1))+ 2;//other
+        }
 
 
+    }
+    switch (structingtypeID) {
+    case 1:
+        buildingtype="W1";
+        break;
+    case 2:
+        buildingtype="S1";
+        break;
+    case 3:
+        buildingtype="S2";
+        break;
+    case 4:
+        buildingtype="C1";
+        break;
+    case 5:
+        buildingtype="C2";
+        break;
+    case 6:
+        buildingtype="C3";
+        break;
+    case 7:
+        buildingtype="RM1";
+        break;
+    case 8:
+        buildingtype="RM2";
+        break;
+    case 9:
+        buildingtype="URM";
+        break;
+    default:
+        break;
+    }
+
+    return buildingtype;
+}
+//double replacementcost(int building_type,double area)
 int main(int argc, const char **argv) {
 
   int minRow = 10;
@@ -34,7 +217,8 @@ int main(int argc, const char **argv) {
 
   std::map<int, locations> parcelLocations;
   std::map<int, locations>::iterator parcelIter;
-  
+  //ofstream opt("ID.txt");
+  //system("md fileBIM");
   //
   // first parse the parcel file, storing parcel location in a map
   //
@@ -48,16 +232,17 @@ int main(int argc, const char **argv) {
   }
   
   const char **headerFields = CsvParser_getFields(header);
-  for (i = 0 ; i < CsvParser_getNumFields(header) ; i++) {
-    ;//    printf("TITLE: %d %s\n", i, headerFields[i]);
-  }    
+  //  for (i = 0 ; i < CsvParser_getNumFields(header) ; i++) {
+  //    printf("TITLE: %d %s\n", i, headerFields[i]);
+  //  }    
+
   CsvRow *row;
   
   while ((row = CsvParser_getRow(csvparser))) {
     const char **rowFields = CsvParser_getFields(row);
-    for (i = 0 ; i < CsvParser_getNumFields(row) ; i++) {
-      ;//   printf("FIELD: %d %s\n", i, rowFields[i]);
-    }
+    // for (i = 0 ; i < CsvParser_getNumFields(row) ; i++) {
+    //   printf("FIELD: %s\n", rowFields[i]);
+    // }
     char *pEnd;
     int parcelID = atoi(rowFields[0]);
     double x = strtod(rowFields[12],&pEnd);
@@ -82,46 +267,44 @@ int main(int argc, const char **argv) {
   
   headerFields = CsvParser_getFields(header);
   for (i = 0 ; i < CsvParser_getNumFields(header) ; i++) {
-    ; //        printf("TITLE: %d %s\n", i, headerFields[i]);
+    //      printf("TITLE: %d %s\n", i, headerFields[i]);
   }
   
-  long currentRow = 1;
+  int currentRow = 1;
   
   json_t *root = json_object();
   
   while ((row = CsvParser_getRow(csvparser))) {
     if (currentRow >= minRow && currentRow <= maxRow) {
-      const char **rowFields = CsvParser_getFields(row);
-      //       for (i = 0 ; i < CsvParser_getNumFields(row) ; i++) {
-      //   printf("FIELD: %d %s\n", i, rowFields[i]);
-      // }
-      
+      const char **rowFields = CsvParser_getFields(row);      
       char *pEnd;
+
       json_t *GI = json_object();
       const char *name = rowFields[0];
       int numStory = atoi(rowFields[10]);
-      double area = strtod(rowFields[8],&pEnd);
 
-      json_object_set(GI,"structType",json_string("UNKNOWN"));
+      double area=strtod(rowFields[8],&pEnd)/10.764/(double)numStory;
+      if (area<=0) area=20;
+      json_object_set(GI,"area",json_real(area));
+
+      //json_object_set(GI,"structType",json_string("UNKNOWN"));
+      const char *type = deterStructtype(atoi(rowFields[11]),atoi(rowFields[15]),atoi(rowFields[10]));
+      //      printf("%s\n",type);
+  
+      json_object_set(GI,"structType",json_string(type));// deterStructtype(atoi(rowFields[11]),atoi(rowFields[15]),atoi(rowFields[10]))));
       json_object_set(GI,"name",json_string(name));
+      //json_object_set(GI,"area",json_real(strtod(rowFields[8],&pEnd)));
       json_object_set(GI,"numStory",json_integer(numStory));
       json_object_set(GI,"yearBuilt",json_integer(atoi(rowFields[11])));
-
-      if (area == 0.0) {
-	area = strtod(rowFields[6],&pEnd);
-	json_object_set(GI,"area",json_real(area));
-	json_object_set(GI,"occupancy",json_string("residence"));
-      } else {
-	json_object_set(GI,"area",json_real(area));
-	json_object_set(GI,"occupancy",json_string("office"));
-      }
       
       // unknown
-
+      //json_object_set(GI,"occupancy",json_string("office"));
+      //deteroccupancy(atoi(rowFields[15]), buildoccupancy, replacementcost);
+      json_object_set(GI,"occupancy",json_string(deteroccupancy(atoi(rowFields[15]))));
       json_object_set(GI,"height",json_real(3.0*numStory));
-      json_object_set(GI,"replacementCost",json_real(12000000.0));
+      json_object_set(GI,"replacementCost",json_real(replacementcost(atoi(rowFields[15]))*strtod(rowFields[8],&pEnd)));
       json_object_set(GI,"replacementTime",json_real(180.0));
-      json_object_set(GI,"structType",json_string("C2"));
+      //json_object_set(GI,"structType",json_string("C2"));
       
       int parcelID = atoi(rowFields[1]);
       parcelIter = parcelLocations.find(parcelID);
@@ -138,17 +321,22 @@ int main(int argc, const char **argv) {
       filename = "exampleBIM.json";
 
       if (argc > 1) {
-          std::ostringstream temp;
-          temp<<currentRow;
-	filename = temp.str() + std::string("-BIM.json");
+	filename = std::string(name) + std::string("-BIM.json");
       }
 	
       // write the file & clean memory
       json_dump_file(root,filename.c_str(),0);
       
+      //opt<<parcelID<<endl;
+      //ss<<"fileBIM\\"<<parcelID<<"-BIM.json";
+      //filename1=ss.str().c_str();
+      //const char *BIMname = filename1.c_str();
+      //json_dump_file(root,BIMname,0);
+
       json_object_clear(root);
       CsvParser_destroy_row(row);
     }
+
 
     currentRow++;
     
