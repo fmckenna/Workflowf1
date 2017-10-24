ebc53ff4fa859d7af7c2cfd944d2115d73a71fb6
fmckenna <fmckenna@berkeley.edu>
2017-10-23 23:56:50 -0700

    fixing output

--- finalProcessing/readDLs.cpp
+++ finalProcessing/readDLs.cpp
@@ -38,28 +38,48 @@ int main(int argc, char **argv) {
     } else {
 
       double loss = 0.;
-      double downtime = 0.;
-      double prob =0.;
+      double medianRepairCost = 0.;
+      double stdRepairCost = 0.;
+      double medianDowntime = 0.;
+      double prob = 0.;
       const char *placard = "none";
+      int placardValue = 0;
+      double pga = 0.0;
 
       json_t *lossO = json_object_get(root,"EconomicLoss"); 
       if (lossO != 0) {
 	loss = json_real_value(json_object_get(lossO,"MedianLossRatio"));
+	medianRepairCost = json_real_value(json_object_get(lossO,"MedianRepairCost"));
+	stdRepairCost = json_real_value(json_object_get(lossO,"StdRepairCost"));
       }
 
       json_t *downtimeO = json_object_get(root,"Downtime");
       if (downtimeO != 0)
-	downtime = json_real_value(json_object_get(downtimeO,"MedianDowntime"));
+	medianDowntime = json_real_value(json_object_get(downtimeO,"MedianDowntime"));
+
+      const char *name;
+
+      json_t *nameO = json_object_get(root,"Name");
+      if (nameO != 0)
+	name = json_string_value(nameO);
+
+      json_t *pgaO = json_object_get(root,"MaxPGA");
+      if (pgaO != 0)
+	pga = json_real_value(pgaO)/9.81;
 
       json_t *placardsO = json_object_get(root,"UnsafePlacards");
       if (placardsO != 0) {
 	placard = json_string_value(json_object_get(placardsO,"Tag"));
+	if (strcmp(placard,"red") == 0) {
+	  placardValue = 1;
+	}
 	prob = json_real_value(json_object_get(placardsO,"RedTagProbability"));
       }
-
       
-      output << loss << "," << downtime << "," << placard << "," << prob << "\n";
-
+      
+      output << name << ",\"UNKNOWN\"," << medianRepairCost << "," << stdRepairCost
+	     << "," << medianDowntime << "," << placardValue << "," << pga << "\n";
+      
       json_object_clear(root);  
     }
   }
